7c469745c20dc63e39a6849a6d818ae04e8f3225
Arturas <asotnicenko@gmail.com>
2018-11-25 02:54:37 +0200

    added conjured item

--- src/main/java/com/gildedrose/model/ConjuredItem.java
+++ src/main/java/com/gildedrose/model/ConjuredItem.java
@@ -9,23 +9,23 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-public class EverydayItem extends Item {
+public class ConjuredItem extends Item {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	Integer id;
 
-	public EverydayItem(String name, @PositiveOrZero(message = "Sell in Should be a positive number") int sellIn,
-			@Min(value = 0, message = "Everyday item quality should not be less than 0") @Max(value = 50, message = "Everyday item quality should not be more than 50") int quality) {
+	public ConjuredItem(String name, @PositiveOrZero(message = "Sell in Should be a positive number") int sellIn,
+			@Min(value = 0, message = "Conjured item quality should not be less than 0") @Max(value = 50, message = "Conjured item quality should not be more than 50") int quality) {
 		super(name, sellIn, quality);
 	}
 
 	@Override
 	public void updateQuality() {
 		if (this.sellIn <= 0 && this.quality >= 0) {
-			this.quality = this.quality - 2;
+			this.quality = this.quality - 4;
 		} else if (this.sellIn > 0) {
 			this.sellIn--;
-			this.quality--;
+			this.quality = this.quality - 2;
 		}
 		if (this.quality < 0) {
 			this.quality = 0;
